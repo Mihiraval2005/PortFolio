4de5ae51033dad64ca16609b23f63eaa715572c6
Mihir Raval <mihiraval2005@gmail.com>
2025-01-10 22:16:30 +0530

    changes_10Jan

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,7 +4,7 @@ import Click_menu from "../../assets/main-home-section.png";
 const index = () => {
   return (
     <div className="contain">
-      <div className="flex">
+      <div className="flex" smooth={true} duration={500}>
         <div className="left" title="Mihir Raval">
           <h2>
             I'am a <span className="highlight">Mihir Raval</span>{" "}
@@ -25,12 +25,37 @@ const index = () => {
         </div>
         <div className="right">
           <div className="img">
-            <img src={Men_char} alt="" />
+            <img src={Men_char} alt="character" />
+          </div>
+        </div>
+      </div>
+      <div className="flex" style={{marginTop:"50px"}}>
+        <div className="left" title="Mihir Raval">
+          <h2>
+            I'am a <span className="highlight">Mihir Raval</span>{" "}
+          </h2>
+
+          <h1>
+            My Proffesion is
+            <span className="highlight"> Frontend Developer</span>{" "}
+          </h1>
+          <p>
+            As a skilled frontend developer, I specialize in creating
+            responsive, user-friendly websites using HTML, CSS, JavaScript, and
+            React. I focus on clean, optimized code and seamless user
+            experiences across devices, leveraging modern technologies and
+            frameworks to bring creative ideas to life.
+          </p>
+          <button className="home-section1-btn">Know More</button>
+        </div>
+        <div className="right">
+          <div className="img">
+            <img src={Men_char} alt="character" />
           </div>
         </div>
       </div>
       <div className="main-section-link">
-        <img src={Click_menu} alt="" />
+        <img src={Click_menu} alt="scroll" />
       </div>
     </div>
   );
